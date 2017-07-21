03e58eed81c65eefe7efd79273e2ab31e267bf46
godloveevin <godloveevin@yeah.net>
2017-07-21 18:03:52 +0800

    rm sandpay api

--- api/src/request/body/clearFileDownload.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace src\request\body;
-
-class clearFileDownload {
-    public $clearDate;//交易日期
-    public $fileType;//文件返回类型
-    public $extend;//扩展域
-}
--- api/src/request/body/orderQuery.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace src\request\body;
-
-class orderQuery {
-    public $orderCode;//商户订单号
-    public $extend;//扩展域
-}
