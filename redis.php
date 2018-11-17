d378f179495701f4b8f5db66e6d97a18f4a89ec5
godloveevin <godloveevin@yeah.net>
2018-11-17 16:59:08 +0800

    add file redis.php

---
+++ redis.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/11/17
+ * Time: 16:49
+ */
+
+$redis = new Redis();
+$redis->connect('127.0.0.1',6379);
+$redis->set('mykey1','hello');
+$redis->set('mykey2','world');
+$value1 = $redis->get('mykey1');
+$value2 = $redis->get('mykey2');
+echo 'mykey1: '.$value1.' and mykey2: '.$value2;
+$redis->close();
