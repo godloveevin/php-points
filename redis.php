f3cac67f93c26be343938699feb83e3fe8217772
godloveevin <godloveevin@yeah.net>
2019-08-07 19:07:51 +0800

    rm some files

--- redis.php
+++
@@ -1,9 +0,0 @@
-<?php
-$redis = new Redis();
-$redis->connect('127.0.0.1',6379);
-$redis->set('mykey1','hello');
-$redis->set('mykey2','world');
-$value1 = $redis->get('mykey1');
-$value2 = $redis->get('mykey2');
-echo 'mykey1: '.$value1.' and mykey2: '.$value2;
-$redis->close();
